b6d344ca50c7e6725e4ba32c4a3d7df849b77953
code-string <fatunbidavidkayode@gmail.com>
2021-05-04 17:28:29 +0100

    rabbitmq hello world message

--- send.py
+++ send.py
@@ -9,8 +9,10 @@ channel = connection.channel()
 # create a queue called 'hello' which message will be delivered
 channel.queue_declare(queue='hello')
 
+# send message to queue via default exchange (identified by '')
 channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
 print(" [x] Sent 'Hello World!'")
 
+# close connection
 connection.close()
 
